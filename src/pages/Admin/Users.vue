a990ecf612d696875c506e48dff3299afbac5556
Felipe blini <Felipe.blini@gmail.com>
2018-08-07 21:55:53 -0300

    customizing table in Users page

--- src/pages/Admin/Users.vue
+++ src/pages/Admin/Users.vue
@@ -1,31 +1,18 @@
 <template>
-    <div class="row">
-      <div class="col-12">
-        <card :title="table1.title" :subTitle="table1.subTitle">
-          <div slot="raw-content" class="table-responsive">
-            <paper-table :data="table1.data" :columns="table1.columns">
+  <div class="row">
+    <div class="col-12">
+      <card :title="table.title" :subTitle="table.subTitle">
+        <div slot="raw-content" class="table-responsive">
+          <paper-table :data="table.data" :columns="table.columns">
 
-            </paper-table>
-          </div>
-        </card>
-      </div>
-
-      <div class="col-12">
-        <card class="card-plain">
-          <div class="table-full-width table-responsive">
-            <paper-table type="hover" :title="table2.title" :sub-title="table2.subTitle" :data="table2.data"
-                         :columns="table2.columns">
-
-            </paper-table>
-          </div>
-        </card>
-      </div>
-
+          </paper-table>
+        </div>
+      </card>
     </div>
+  </div>
 </template>
 <script>
 import { PaperTable } from "@/components";
-const tableColumns = ["Id", "Name", "Salary", "Country", "City"];
 const tableData = [
   {
     id: 1,
@@ -70,16 +57,10 @@ export default {
   },
   data() {
     return {
-      table1: {
-        title: "Stripped Table",
+      table: {
+        title: "Usuários Cadastrados no Sistema",
         subTitle: "Here is a subtitle for this table",
-        columns: [...tableColumns],
-        data: [...tableData]
-      },
-      table2: {
-        title: "Table on Plain Background",
-        subTitle: "Here is a subtitle for this table",
-        columns: [...tableColumns],
+        columns: ["Id", "Name", "Salary", "Country", "City"],
         data: [...tableData]
       }
     };
