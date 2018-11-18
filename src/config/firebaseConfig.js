f49fcf242d1191f06cd5c12716d672f68f8e69fe
Felipe blini <Felipe.blini@gmail.com>
2018-11-18 00:41:58 -0200

    checking serviceWorker support before its registration

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -12,9 +12,14 @@ export const initializeFirebase = () => {
   firebase.initializeApp(config);
 
   if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("/stresscan-sw.js").then(registration => {
-      console.log(registration);
-      firebase.messaging().useServiceWorker(registration);
-    });
+    navigator.serviceWorker
+      .register("/stresscan-sw.js")
+      .then(registration => {
+        console.log(registration);
+        firebase.messaging().useServiceWorker(registration);
+      })
+      .catch(err => console.log("registration failed", err));
+  } else {
+    console.log("no support for sw");
   }
 };
